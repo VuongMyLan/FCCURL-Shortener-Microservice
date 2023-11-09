c6603f27fc1193c799a0e931957630fb10b36cfd
vuongmylan <vuongmylan2708@gmail.com>
2023-11-09 16:54:41 +0700

    URL Shortener Microservice

--- index.js
+++ index.js
@@ -4,21 +4,55 @@ const cors = require('cors');
 const app = express();
 
 // Basic Configuration
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/', function (req, res) {
+    res.sendFile(process.cwd() + '/views/index.html');
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.get('/api/hello', function (req, res) {
+    res.json({ greeting: 'hello API' });
 });
 
-app.listen(port, function() {
-  console.log(`Listening on port ${port}`);
+const shortURL = {};
+let number = 1;
+app.post('/api/shorturl', (req, res) => {
+    let url = req.body.url;
+    const regex = new RegExp(
+        /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-_]*)?\??(?:[\-\+=&;%@\.\w_]*)#?(?:[\.\!\/\\\w]*))?)/
+    );
+    if (url.match(regex)) {
+        // url = new URL(req.body.url);
+        shortURL[number] = url;
+
+        res.json({
+            original_url: url,
+            short_url: number,
+        });
+        number += 1;
+    } else {
+        res.json({ error: 'invalid url' });
+        return;
+    }
+});
+
+app.get('/api/shorturl/:id', (req, res) => {
+    const id = req.params.id;
+    console.log(id);
+    let url = shortURL[id];
+    console.log(shortURL);
+    console.log(shortURL[id]);
+    res.redirect(url);
+});
+
+app.listen(port, function () {
+    console.log(`Listening on port ${port}`);
 });
